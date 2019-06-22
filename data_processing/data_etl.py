6ebf558e34c0ede29dbcbab2f5217784e1c871e8
smileykaur <akaur4@sdsu.edu>
2019-06-21 18:38:19 -0700

    adding flask

--- data_processing/data_etl.py
+++ data_processing/data_etl.py
@@ -1,1 +1,8 @@
-# TODO: Set Spark Master default config for execution
+# TODO: Import data from S3
+# TODO: Extract sub set of columns
+# TODO: Perform data cleaning - remove Deleted, Removed
+# TODO: Load the ETL data to ElasticSearch
+# TODO: Build NLP Transformation
+# TODO: Upload clean data to S3
+# TOOD: Generate word count
+#
