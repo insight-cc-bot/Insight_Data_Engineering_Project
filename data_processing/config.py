b987d8f9f5f5816cff6ef4bd7e2d4aeba246db4d
Amanpreet Kaur <32923161+smileykaur@users.noreply.github.com>
2019-06-23 11:06:01 -0700

    config file

---
+++ data_processing/config.py
@@ -0,0 +1,5 @@
+RAW_COMMENTS_BUCKET = "reddit-comments-raw"
+CLEAN_COMMENTS_BUCKET = "reddit-comments-clean"
+RAW_SUBMISSIONS_BUCKET = "reddit-submissions-raw"
+CLEAN_SUBMISSIONS_BUCKET = "reddit-submissions-clean"
+OUTPUT_DIRECTORY = "./Data"
